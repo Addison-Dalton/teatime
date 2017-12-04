12214a2e3c035bc94643cb7324000296ba2b458f
Addison Dalton <zarch14@gmail.com>
2017-12-04 15:27:51 -0500

    added method to convert milliseconds to MM:SS, and a method to return a string containing the name and time of a TeaProfile object

--- app/src/main/java/com/addisondalton/teatime/TeaProfile.java
+++ app/src/main/java/com/addisondalton/teatime/TeaProfile.java
@@ -2,6 +2,9 @@ package com.addisondalton.teatime;
 
 import com.orm.SugarRecord;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Owner on 12/4/2017.
  */
@@ -18,5 +21,15 @@ public class TeaProfile extends SugarRecord<TeaProfile> {
     public TeaProfile(String name, int milliseconds){
         this.name = name;
         this.milliseconds = milliseconds;
+    }
+
+    public String getFullString(){
+        return (this.name + ", " + getMinutesAndSeconds());
+    }
+
+    private String getMinutesAndSeconds(){
+        return String.format(Locale.US, "%2d:%02d", TimeUnit.MILLISECONDS.toMinutes(this.milliseconds),
+                                                                                TimeUnit.MILLISECONDS.toSeconds(this.milliseconds) -
+                                                                                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(this.milliseconds)));
     }
 }
